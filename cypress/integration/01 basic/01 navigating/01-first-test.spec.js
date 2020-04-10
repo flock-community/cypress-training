145ad232038454d38b6f5e45caa21c17c218f0e3
Reinier Boon <reinier@reinierboon.com>
2020-04-10 14:58:00 +0200

    chore: retructure tests

---
+++ cypress/integration/01 basic/01 navigating/01-first-test.spec.js
@@ -0,0 +1,5 @@
+describe("The Home Page", () => {
+  it("successfully loads", () => {
+    // TODO: implement me!
+  });
+});
--- cypress/integration/1-first-test.spec.js
+++
@@ -1,9 +0,0 @@
-describe("The Home Page", () => {
-  it("successfully loads", () => {
-    // TODO: implement me!
-    cy.visit("example.cypress.io");
-    cy.get("html");
-    cy.get(".container");
-    cy.get("h1").contains("Kitchen Sink");
-  });
-});
